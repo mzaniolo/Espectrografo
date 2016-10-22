ffa397c38513cfddea8bc13644018d29fdbe3623
root <root@matheus>
2016-10-21 22:40:51 -0200

    Estrutura inicial. Main + def

---
+++ Sources/Definitions.h
@@ -0,0 +1,29 @@
+/*
+ * Definitions.h
+ *
+ *  Created on: 18/10/2016
+ *      Author: root
+ */
+
+#ifndef SOURCES_DEFINITIONS_H_
+#define SOURCES_DEFINITIONS_H_
+
+/* system includes */
+#include <MKL25Z4.h>
+
+/*                 General uC definitions                 */
+
+/* Clock gate control */
+#define  CGC_CLOCK_DISABLED         0x00U
+#define  CGC_CLOCK_ENABLED          0x01U
+
+/* GPIO input / output */
+#define GPIO_INPUT                  0x00U
+#define GPIO_OUTPUT                 0x01U
+
+
+/*                 END OF General uC definitions         */
+
+
+
+#endif /* SOURCES_DEFINITIONS_H_ */
